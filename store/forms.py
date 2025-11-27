02c616b2ffaa34c481adc325e929f0ae3b80dfb9
juan081201 <jdanielherrera08@gmail.com>
2025-11-27 08:41:16 -0800

    Agregue nuaV

--- store/forms.py
+++ store/forms.py
@@ -7,14 +7,14 @@ from .models import Item
 class LoginForm(AuthenticationForm):
     username = forms.CharField(widget=forms.TextInput(
         attrs={
-            'placeholder': 'Tu username',
+            'placeholder': 'Tu usuario',
             'class': 'form-control'
         }
     ))
 
     password = forms.CharField(widget=forms.PasswordInput(
         attrs={
-            'placeholder': 'Tu password',
+            'placeholder': 'password',
             'class': 'form-control'
         }
     ))
@@ -22,11 +22,11 @@ class LoginForm(AuthenticationForm):
 class SignupForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2' ]
+        fields = ('username', 'email', 'password1', 'password2')
 
     username = forms.CharField(widget=forms.TextInput(
         attrs={
-            'placeholder': 'Tu Username',
+            'placeholder': 'Tu Usuario',
             'class': 'form-control'
         }
     ))
@@ -47,7 +47,36 @@ class SignupForm(UserCreationForm):
 
     password2 = forms.CharField(widget=forms.PasswordInput(
         attrs={
-            'placeholder': 'Repite Tu Password',
+            'placeholder': 'Repite Password',
             'class': 'form-control'
         }
     ))
+
+class NewItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ('category', 'name', 'description', 'price', 'image',)
+
+        widgets = {
+            'category': forms.Select(attrs={
+                'class': 'form-select'
+            }),
+            'name': forms.TextInput(attrs={
+                'class': 'form-control'
+            }),
+            'description': forms.Textarea(attrs={
+                'class': 'form-control',
+                'style': 'height: 100px'
+            }),
+            'price': forms.TextInput(attrs={
+                'class': 'form-control',
+            }),
+            'price': forms.TextInput(attrs={
+                'class': 'form-control',
+            }),
+            'image': forms.FileInput(attrs={
+                'class': 'form-control',
+            }),
+        }
+
+
--- store/views.py
+++ store/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+
 from .models import Item, Category
 
-from .forms import SignupForm
- 
+from .forms import SignupForm, NewItemForm
 
 # Create your views here.
 def home(request):
@@ -25,9 +26,7 @@ def contact(request):
 
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
-    related_items = Item.objects.filter(category=item.category, 
-                                        is_sold=False).exclude(pk=pk)[0:3]
-
+    related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)[0:3]
     context={
         'item': item,
         'related_items': related_items
@@ -44,9 +43,34 @@ def register(request):
             return redirect('login')
     else:
         form = SignupForm()
-    
+
     context = {
         'form': form
     }
 
     return render(request, 'store/signup.html', context)
+
+def logout_user(request):
+    logout(request)
+
+    return redirect('home')
+
+@login_required
+def add_item(request):
+    if request.method == 'POST':
+        form = NewItemForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.created_by = request.user
+            item.save()
+
+            return redirect('detail', pk=item.id)
+    else:
+        form = NewItemForm()
+        context = {
+            'form': form,
+            'title': 'New Item'
+        }
+
+    return render(request, 'store/form.html', context)
